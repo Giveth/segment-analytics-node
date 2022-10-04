aabcf34ff159c7fca9e1246b870e747bde62593c
Carlos Quintero <cquinterom096@outlook.com>
2022-10-04 01:26:21 -0500

    fix test import

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { assert } from "chai";
-import SegmentAnalytics from "./index";
+const SegmentAnalytics = require("./index");
 
 describe("segmentAnalytics test cases --->", SegmentAnalyticsTestCases);
 
